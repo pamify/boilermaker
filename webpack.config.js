9179b24ddc3665d3504a7ce42a44cf920eb9981c
Pamela Jung <pamjung@gmail.com>
2022-02-22 19:48:07 -0500

    complete CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+// module.exports = {
+//   mode: 'development',
+//   entry: './client/index.js',
+//   output: {
+//     path: __dirname,
+//     filename: './public/bundle.js'
+//   },
+//   devtool: 'source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         options: {
+//           presets: [
+//             '@babel/preset-react'
+//           ]
+//         }
+//       }
+//     ]
+//   }
+// }
+
 module.exports = {
   mode: 'development',
   entry: './client/index.js',
@@ -10,15 +34,20 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: [
-            '@babel/preset-react'
-          ]
+          presets: ['react', 'es2015']
         }
+      },
+      // use the style-loader/css-loader combos for anything matching the .css extension
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
       }
     ]
   }
-}
-
+};
